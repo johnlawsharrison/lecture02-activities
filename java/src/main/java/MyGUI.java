4e4168dca387e95fa2207f7407063c27def178e1
John Laws Harrison <johnlawsharrison@gmail.com>
2017-03-30 13:55:51 -0700

    Add listener

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -13,16 +13,18 @@ public class MyGUI extends JFrame {
 		
 		JButton theButton = new JButton("Click me!");
 
+		theButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("CLICKAROO");
+			}
+		});
 
-
-
-		
 		this.add(theButton);
 		
 		this.pack();
 		this.setVisible(true);		
 	}
-	
+
 	public static void main(String[] args) {
 		new MyGUI(); //create the frame
 	}
